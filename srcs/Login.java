ca3e0e02599341e882e6d07a9280cba8893bb4d4
hara ryosei <aaxouei52@gmail.com>
2023-07-11 13:53:24 +0900

    add Login

--- srcs/Login.java
+++ srcs/Login.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.JLabel;
+import javax.swing.*;
 
 public class Login extends BasePage {
 	public Login(BasePage page) { super(page); }
@@ -19,7 +19,25 @@ public class Login extends BasePage {
 		JLabel label = panel.createLabel("Login", 0.45, 0.1, 0.3, 0.05);
 		label.setFont(new Font("Arial", Font.PLAIN, 20));
 
+		JLabel username_label = panel.createLabel("Username: ", 0.05, 0.2, 0.15, 0.05);
+		JTextField _username_field = panel.createTextField("", 0.2, 0.2, 0.6, 0.05);
+		JLabel password_label = panel.createLabel("password: ", 0.05, 0.3, 0.15, 0.05);
+		JTextField _password_field = panel.createPasswordField("", 0.2, 0.3, 0.6, 0.05);
+
 		panel.add(label);
+		panel.add(username_label);
+		panel.add(_username_field);
+		panel.add(password_label);
+		panel.add(_password_field);
+
+		Action action = new Action();
+		JButton button = panel.createButton("Back SignUp", 0.2, 0.8, 0.2, 0.1);
+		button.addActionListener(action);
+		panel.add(button);
+
+		JButton button2 = panel.createButton("Login", 0.5, 0.8, 0.2, 0.1);
+		button2.addActionListener(action);
+		panel.add(button2);
 
 		return (panel);
 	}
