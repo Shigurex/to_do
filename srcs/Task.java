a341ec4a3e6d109b1e5b86253f31df90c9b7f1b2
Yamato Hokari <yamatohokari@gmail.com>
2023-07-12 17:56:54 +0900

    task add

--- srcs/Task.java
+++ srcs/Task.java
@@ -13,10 +13,12 @@ public class Task extends BasePage {
 			String cmd = e.getActionCommand();
 			BasePage page = null;
 
-			if (cmd.startsWith("task_")) {
+			if (cmd.startsWith("Task_")) {
 				int task_id = Integer.valueOf(cmd.substring(5));
 				page = new ToDo(Task.this, task_id);
-			} else
+			} else if (cmd.equals("Task Add"))
+				page = new TaskAdd(Task.this);
+			else
 				page = new Error(Task.this);
 
 			if (page != null)
@@ -35,13 +37,17 @@ public class Task extends BasePage {
 
 		Action action = new Action();
 
+		JButton task_add_button = panel.createButton("Task Add", 0.65, 0.1, 0.1, 0.05);
+		task_add_button.addActionListener(action);
+		panel.add(task_add_button);
+
 		ArrayList<ArrayList<String>> info = SQL.select("select ta.id, ta.name from member m, task ta where ta.owner=m.id and m.id=?;", 2, String.valueOf(this._frame.getLoginId()));
 		for (int i = 0; i < info.size(); i++) {
 			ArrayList<String> str_list = info.get(i);
 			String task_id = str_list.get(0);
 			String task_name = str_list.get(1);
 			JButton task_button = panel.createButton(task_name, 0.2, 0.2 + i * 0.05, 0.6, 0.05);
-			task_button.setActionCommand("task_" + task_id);
+			task_button.setActionCommand("Task_" + task_id);
 			task_button.addActionListener(action);
 			panel.add(task_button);
 		}
---
+++ srcs/TaskAdd.java
@@ -0,0 +1,95 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TaskAdd extends BasePage {
+	private JTextField _name_field;
+	private JLabel _name_error;
+	private JTextField _description_field;
+	private JLabel _description_error;
+
+	public TaskAdd(BasePage page) { super(page); }
+	public TaskAdd(BaseFrame frame) { super(frame); }
+
+	public class Action implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			String cmd = e.getActionCommand();
+			BasePage page = null;
+
+			if (cmd.equals("Create Button"))
+				page = checkField();
+			else
+				page = new Error(TaskAdd.this);
+
+			if (page != null)
+				TaskAdd.this._frame.changePanel(page.createPage());
+		}
+
+		public BasePage checkField() {
+			String name = _name_field.getText();
+			String description = _description_field.getText();
+			if (!isValid(name, description))
+				return (null);
+			addTask(name, description, false, false);
+			return (new Task(TaskAdd.this));
+		}
+
+		public void addTask(String name, String description, boolean is_public, boolean is_archive) {
+			SQL.insert("insert into task (owner, name, description, is_public, is_archive) VALUES(?, ?, ?, ?, ?)", String.valueOf(TaskAdd.this._frame.getLoginId()), name, description, is_public ? "1" : "0", is_archive ? "1" : "0");
+		}
+
+		public boolean isValid(String name, String description) {
+			boolean is_valid = true;
+			if (name.equals("")) {
+				_name_error.setText("Please input task name");
+				is_valid = false;
+			}
+			else
+				_name_error.setText("");
+			if (description.equals("")) {
+				_description_error.setText("Please input description");
+				is_valid = false;
+			}
+			else
+				_description_error.setText("");
+			return (is_valid);
+		}
+	}
+
+	public BasePanel createPage() {
+		BasePanel panel = new BasePanel(this._frame);
+		panel.is_menu = true;
+		panel.setLayout(null);
+
+		JLabel label = panel.createLabel("TaskAdd", 0.45, 0.1, 0.1, 0.05);
+		label.setFont(new Font("Arial", Font.PLAIN, 20));
+		panel.add(label);
+
+		JLabel name_label = panel.createLabel("Name: ", 0.05, 0.2, 0.15, 0.05);
+		this._name_field = panel.createTextField("", 0.2, 0.2, 0.6, 0.05);
+		this._name_error = panel.createLabel("",0.2, 0.25, 0.6, 0.05);
+		this._name_error.setForeground(Color.RED);
+		JLabel description_label = panel.createLabel("description: ", 0.05, 0.3, 0.15, 0.05);
+		this._description_field = panel.createTextField("", 0.2, 0.3, 0.6, 0.05);
+		this._description_error = panel.createLabel("",0.2, 0.35, 0.6, 0.05);
+		this._description_error.setForeground(Color.RED);
+
+		panel.add(name_label);
+		panel.add(this._name_field);
+		panel.add(this._name_error);
+		panel.add(description_label);
+		panel.add(this._description_field);
+		panel.add(this._description_error);
+
+		Action action = new Action();
+
+		JButton button = panel.createButton("Create Button", 0.2, 0.8, 0.2, 0.1);
+		button.addActionListener(action);
+		panel.add(button);
+
+		return (panel);
+	}
+}
