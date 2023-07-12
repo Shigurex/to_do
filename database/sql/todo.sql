02089249dc54d0805c2e57b5f8bea7a2fdba0da3
Yamato Hokari <yamatohokari@gmail.com>
2023-07-12 11:31:27 +0900

    sql modified

--- database/sql/todo.sql
+++ database/sql/todo.sql
@@ -8,4 +8,4 @@ CREATE TABLE todo (
 	update_time TIMESTAMP
 );
 
-INSERT INTO todo (task, title, deadline, create_time, update_time) VALUES (1, 'java final report', '2023/07/23 23:50:00', strftime('%Y/%m/%d %H:%M:%S', CURRENT_TIMESTAMP), strftime('%Y/%m/%d %H:%M:%S', CURRENT_TIMESTAMP));
+INSERT INTO todo (task, title, deadline, create_time, update_time) VALUES (1, 'java final report', '2023/07/23 23:50:00', strftime('%Y/%m/%d %H:%M:%S', CURRENT_TIMESTAMP, 'localtime'), strftime('%Y/%m/%d %H:%M:%S', CURRENT_TIMESTAMP, 'localtime'));
