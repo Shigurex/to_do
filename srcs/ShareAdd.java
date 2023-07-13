ff23ec7d1a8fd170fb3e9d71b136fe686abf677b
hara ryosei <aaxouei52@gmail.com>
2023-07-13 20:50:38 +0900

    Share add

--- srcs/ShareAdd.java
+++ srcs/ShareAdd.java
@@ -3,31 +3,38 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class Users extends BasePage {
-	private static ArrayList<ArrayList<String>> search_list;
+public class ShareAdd extends BasePage {
+	private static ArrayList<ArrayList<String>> search_list = null;
+	private int task_id;
 	private JTextField username_field;
 	private JLabel username_error;
 
-	public Users(BasePage page) { super(page); }
-	public Users(BaseFrame frame) { super(frame); }
+	public ShareAdd(BasePage page) { super(page); }
+	public ShareAdd(BaseFrame frame) { super(frame); }
+	public ShareAdd(BasePage page,int task_id) {
+		super(page);
+		this.task_id = task_id;
+	}
 
 	public class Action implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			String cmd = e.getActionCommand();
 			BasePage page = null;
 
-			if (cmd.equals("Search"))
-				page = searchUser();
+			if (cmd.equals("Back to Share"))
+				page = new ToDo(ShareAdd.this, task_id);
 			else if (cmd.startsWith("User_")) {
-				page = new UserInfo(Users.this, cmd.substring(5));
-				search_list = null;
+				String member_name = cmd.substring(5);
+				//insert;
+				page = new ShareAdd(ShareAdd.this, ShareAdd.this.task_id);
 			}
+			else if (cmd.equals("Search"))
+				page = searchUser();
 			else
-				page = new Error(Users.this);
+				page = new Error(ShareAdd.this);
+
 			if (page != null)
-				Users.this._frame.changePanel(page.createPage());
-			else
-				search_list = null;
+				ShareAdd.this._frame.changePanel(page.createPage());
 		}
 
 		public BasePage searchUser() {
@@ -37,17 +44,16 @@ public class Users extends BasePage {
 			if (username.equals("")) {
 				username_error.setText("Please input username");
 				search_list = null;
-				return (null);
+				return (new ShareAdd(ShareAdd.this, task_id));
 			}
 			else {
 				search_list = SQL.select_like("SELECT name FROM member WHERE is_public=1 and name LIKE ?", 1, username);
 				if (search_list.size() == 0) {
 					search_list = null;
-					return (null);
+					return (new ShareAdd(ShareAdd.this, task_id));
 				}
 				else
-					return (new Users(Users.this));
-
+					return (new ShareAdd(ShareAdd.this, task_id));
 			}
 		}
 	}
@@ -57,14 +63,18 @@ public class Users extends BasePage {
 		panel.is_menu = true;
 		panel.setLayout(null);
 
-		JLabel label = panel.createLabel("Users", 0.45, 0.1, 0.1, 0.05);
+		JLabel label = panel.createLabel("Share Add", 0.35, 0.1, 0.3, 0.05);
 		label.setFont(new Font("Arial", Font.PLAIN, 20));
-		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(label);
 
 		JLabel username_label = panel.createLabel("Username Search: ", 0.05, 0.2, 0.15, 0.05);
 		username_field = panel.createTextField("", 0.2, 0.2, 0.5, 0.05);
 		username_error = panel.createLabel("",0.2, 0.25, 0.6, 0.05);
 		username_error.setForeground(Color.RED);
+
+		panel.add(username_field);
+		panel.add(username_error);
 
 		Action action = new Action();
 
@@ -72,21 +82,28 @@ public class Users extends BasePage {
 		button.addActionListener(action);
 		panel.add(button);
 
+		double panel_height = 0.3;
 		if (search_list != null) {
 			for (int i = 0; i < search_list.size(); i++) {
 				ArrayList<String> str_list = search_list.get(i);
 				String username = str_list.get(0);
-				JButton search_ans = panel.createButton(username, 0.35, 0.3 + i*0.06, 0.3, 0.05);
+				JLabel search_label = panel.createLabel(username, 0.3, panel_height, 0.2, 0.05);
+				JButton search_ans = panel.createButton("set Viewable", 0.55, panel_height, 0.15, 0.05);
 				search_ans.setActionCommand("User_" + username);
 				search_ans.addActionListener(action);
+				panel.add(search_label);
 				panel.add(search_ans);
+
+				panel_height = panel_height + 0.06;
+				if (panel_height > 0.8)
+					panel.setPreferredSize(new Dimension(900, (int)(ShareAdd.this._frame.getWinHeight() * (panel_height + 0.2))));
 			}
+
 		}
 
-		panel.add(label);
-		panel.add(username_label);
-		panel.add(username_field);
-		panel.add(username_error);
+		JButton button2 = panel.createButton("Back to Share", 0.4, panel_height + 0.05, 0.2, 0.05);
+		button2.addActionListener(action);
+		panel.add(button2);
 
 		return (panel);
 	}
