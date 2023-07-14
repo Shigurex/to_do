dd180797fc7b145ab652e5024c18d036d1b8b459
hara ryosei <aaxouei52@gmail.com>
2023-07-14 11:04:22 +0900

    sql test_data add

--- database/sql/share.sql
+++ database/sql/share.sql
@@ -2,8 +2,15 @@ DROP TABLE IF EXISTS share;
 CREATE TABLE share (
 	task INTEGER,
 	member INTEGER,
-	is_editable BOOLEAN default false
+	is_editable BOOLEAN default false,
+	is_archive BOOLEAN default false
 );
 
-INSERT INTO share (task, member, is_editable) VALUES (1, 2, false);
-INSERT INTO share (task, member, is_editable) VALUES (1, 3, false);
+INSERT INTO share (task, member, is_editable) VALUES (4, 3, true);
+INSERT INTO share (task, member, is_editable) VALUES (5, 3, true);
+INSERT INTO share (task, member, is_editable) VALUES (6, 4, false);
+INSERT INTO share (task, member, is_editable) VALUES (7, 5, false);
+INSERT INTO share (task, member, is_editable) VALUES (8, 6, false);
+INSERT INTO share (task, member, is_editable) VALUES (9, 7, false);
+INSERT INTO share (task, member, is_editable) VALUES (10, 8, false);
+INSERT INTO share (task, member, is_editable) VALUES (11, 9, false);
--- database/sql/task.sql
+++ database/sql/task.sql
@@ -4,8 +4,24 @@ CREATE TABLE task (
 	owner INTEGER,
 	name TEXT,
 	description TEXT,
-	is_public BOOLEAN,
-	is_archive BOOLEAN
+	is_archive BOOLEAN default false
 );
 
-INSERT INTO task (owner, name, description, is_public, is_archive) VALUES (1, 'school', 'task about school is written', false, false);
+INSERT INTO task (owner, name, description) VALUES (1, 'School', 'task about school is written');
+INSERT INTO task (owner, name, description) VALUES (1, 'Work', 'task about Work is written');
+INSERT INTO task (owner, name, description) VALUES (1, 'Hobby', 'task about Hobby is written');
+INSERT INTO task (owner, name, description) VALUES (2, 'School', 'task about School is written');
+INSERT INTO task (owner, name, description) VALUES (2, 'Work', 'task about Work is written');
+INSERT INTO task (owner, name, description) VALUES (2, 'School', 'task about School is written');
+INSERT INTO task (owner, name, description) VALUES (3, 'School', 'task about School is written');
+INSERT INTO task (owner, name, description) VALUES (3, 'Club Activity', 'task about Club Activity is written');
+INSERT INTO task (owner, name, description) VALUES (3, 'Part-time Job', 'task about Part-time Job is written');
+INSERT INTO task (owner, name, description) VALUES (4, 'School', 'task about school is written');
+INSERT INTO task (owner, name, description) VALUES (4, 'Work', 'task about Work is written');
+INSERT INTO task (owner, name, description) VALUES (4, 'Hobby', 'task about Hobby is written');
+INSERT INTO task (owner, name, description) VALUES (5, 'School', 'task about school is written');
+INSERT INTO task (owner, name, description) VALUES (5, 'Work', 'task about Work is written');
+INSERT INTO task (owner, name, description) VALUES (5, 'Hobby', 'task about Hobby is written');
+INSERT INTO task (owner, name, description) VALUES (6, 'School', 'task about school is written');
+INSERT INTO task (owner, name, description) VALUES (6, 'Work', 'task about Work is written');
+INSERT INTO task (owner, name, description) VALUES (6, 'Hobby', 'task about Hobby is written');
